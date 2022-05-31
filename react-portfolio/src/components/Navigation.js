06ef7526f386faa23733fa6ad0cb421337c434a0
mateonav98 <mateonav98@gmail.com>
2022-05-31 07:20:23 -0700

    adding nav bar

---
+++ react-portfolio/src/components/Navigation.js
@@ -0,0 +1,19 @@
+const Navigation = () => {
+  return (
+    <nav>
+    <ul>
+      <li>
+        <a href="#cards-container">Projects</a>
+      </li>
+      <li>
+        <a href="#About">About</a>
+      </li>
+      <li>
+        <a href="#Contact">Contact</a>
+      </li>
+    </ul>
+  </nav>
+);
+}
+
+export default Navigation
