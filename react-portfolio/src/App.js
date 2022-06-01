27e95ca878c2530bf551cee33eff30fa6101ef24
mateonav98 <mateonav98@gmail.com>
2022-06-01 08:05:37 -0700

    adding about me section

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
 import Projects from "./components/Projects";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="react-portfolio">
       <Header />
       <Navigation />
+      <About />
       <Projects />
     </div>
   );
