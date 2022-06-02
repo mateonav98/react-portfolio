592c007f328eabefeca681747c557ffe7426e0bd
mateonav98 <mateonav98@gmail.com>
2022-06-01 18:28:55 -0700

    add layout folder

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,14 +1,19 @@
 import './App.scss';
+import {Routes} from 'react-router-dom'
 import Header from "./components/Header";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Resume from "./components/Resume";
 import Contact from "./components/Contact";
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="react-portfolio">
       <Header />
+      <Routes>
+        <Route path="/" element= {<Layout />} />
+      </Routes>
 
     </div>
   );
