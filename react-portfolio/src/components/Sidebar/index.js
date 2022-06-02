5ebed176a91a4d1526e67c75735b9fc4915d3fb2
mateonav98 <mateonav98@gmail.com>
2022-06-01 20:42:16 -0700

    add socials with button

--- react-portfolio/src/components/Sidebar/index.js
+++ react-portfolio/src/components/Sidebar/index.js
@@ -1,7 +1,8 @@
 import './index.scss'
 import {Link, NavLink} from "react-router-dom"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faHome, faUser} from '@fortawesome/free-solid-svg-icons'
+import {faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => {
   return (
@@ -19,6 +20,24 @@ const Sidebar = () => {
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a target='_blank' 
+                rel='noreferrer' 
+                href='https://www.linkedin.com/in/mateonav/'>
+                <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+
+                </a>
+            </li>
+            <li>
+                <a target='_blank' 
+                rel='noreferrer' 
+                href='https://github.com/mateonav98'>
+                <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+
+                </a>
+            </li>
+        </ul>
     </div>
   )
 }
